be3bdb03bded0caacf80c350e8ffd51e48fb52b6
Buongiorno <mnbuongiorno@gmail.com>
2024-04-10 20:55:25 -0300

    Added spawning player + player movement logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
+import { displayDialogue } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -22,7 +23,7 @@ k.scene("main", async () => {
     const mapData = await (await fetch("./map.json")).json()
     const layers = mapData.layers;
 
-    const map = k.make([
+    const map = k.add([
         k.sprite("map"),
         k.pos(0),
         k.scale(scaleFactor)
@@ -47,7 +48,54 @@ k.scene("main", async () => {
 
     ]);
     
-    k.add
+    for (const layer of layers) {
+        if (layer.name === "boundaries"){
+            for (const boundary of layer.objects) {
+                map.add([
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+                    }),
+                    k.body({ isStatic: true}),
+                    k.pos(boundary.x, boundary.y),
+                    boundary.name
+                ]);
+
+                if (boundary.name) {
+                    player.onCollide(boundary.name, () => {
+                        player.isInDialogue = true;
+                        displayDialogue("TODO", () => (player.isInDialogue = false))
+                    })
+                }
+            }
+            continue;
+        }
+
+        if(layer.name === "spawnpoints") {
+            for (const entity of layer.objects) {
+                if (entity.name === "player") {
+                    player.pos = k.vec2(
+                        (map.pos.x + entity.x) * scaleFactor,
+                        (map.pos.y + entity.y) * scaleFactor
+                    );
+                    k.add(player);
+                    continue;
+                }
+            }
+        }
+    }
+
+    
+
+    k.onUpdate(() => {
+        k.camPos(player.pos.x, player.pos.y + 100);
+    });
+
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn !== "left" || player.isInDialogue) return;
+
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed); 
+    })
 });
 
 k.go("main");
